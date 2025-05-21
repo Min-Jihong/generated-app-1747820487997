d8af62798dc1956b7359dac678fde280532d475f
Min-Jihong <69140228+Min-Jihong@users.noreply.github.com>
2025-05-21 18:42:35 +0900

    Add ProductDetailPage page

---
+++ src/app/productdetailpage/page.tsx
@@ -0,0 +1,9 @@
+import { ProductDetailPage } from "@/components/ProductDetailPage";
+
+export default function ProductDetailPagePage() {
+  return (
+    <main className="container mx-auto p-4">
+      <ProductDetailPage />
+    </main>
+  );
+}
