41855fcd279282e5e76efa8abbafc4bc485054a7
Min-Jihong <69140228+Min-Jihong@users.noreply.github.com>
2025-05-21 18:42:34 +0900

    Add LoginForm page

---
+++ src/app/loginform/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/LoginForm";
+
+export default function LoginFormPage() {
+  return (
+    <main className="container mx-auto p-4">
+      <LoginForm />
+    </main>
+  );
+}
